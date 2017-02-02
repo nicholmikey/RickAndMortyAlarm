2d8b1a8c2a6f09e31642a65d99109c6d91bbd623
nicholmikey <mike.nichols@gmail.com>
2017-02-02 12:13:04 -0500

    Add windows app

---
+++ WindowsApp/RickMortyAlarm/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace RickMortyAlarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtSeconds = new System.Windows.Forms.TextBox();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSeconds
+            // 
+            this.txtSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSeconds.Location = new System.Drawing.Point(367, 89);
+            this.txtSeconds.Name = "txtSeconds";
+            this.txtSeconds.Size = new System.Drawing.Size(100, 68);
+            this.txtSeconds.TabIndex = 0;
+            this.txtSeconds.Text = "00";
+            this.txtSeconds.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinutes.Location = new System.Drawing.Point(245, 89);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(100, 68);
+            this.txtMinutes.TabIndex = 1;
+            this.txtMinutes.Text = "00";
+            this.txtMinutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtHours
+            // 
+            this.txtHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHours.Location = new System.Drawing.Point(120, 89);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(102, 68);
+            this.txtHours.TabIndex = 2;
+            this.txtHours.Text = "00";
+            this.txtHours.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(484, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 68);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(610, 235);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.txtSeconds);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Rick and Morty Alarm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSeconds;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsApp/RickMortyAlarm/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RickMortyAlarm
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool HideCaret(IntPtr hWnd);
+        public Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            var timer = new Timer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = 1000; //10 seconds
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = 1000; //10 seconds
+            timer.Start();
+            HideCaret(txtSeconds.Handle);
+        }
+
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+
+            int secondsLeft = Int32.Parse(txtSeconds.Text);
+            if (secondsLeft > 0)
+            {
+                secondsLeft--;
+                txtSeconds.Text = secondsLeft.ToString();
+            }
+            else if (secondsLeft == 0)
+            {
+                timer.Stop();
+
+                //triggerArduino
+
+            }
+            if (secondsLeft == 1) {
+                PlaySun();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PlaySun()
+        {
+            using (SerialPort serialPort1 = new SerialPort())
+            {
+                serialPort1.PortName = "COM4"; //set the port name you see in arduino IDE
+                serialPort1.BaudRate = 9600;   //set the Baud you see in arduino IDE
+
+                serialPort1.Open();
+
+                System.Threading.Thread.Sleep(300);
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine("a");
+                    serialPort1.Close();
+                }
+            }
+
+        }
+    }
+}
